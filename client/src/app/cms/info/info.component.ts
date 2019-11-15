eb161bc60eb4f41aef3b6964ed6a18ba22b8a994
JSebastianCK <jsebastianck@gmail.com>
2019-11-14 23:37:42 -0300

    Se muestra solo el ganador ahora

--- client/src/app/cms/info/info.component.ts
+++ client/src/app/cms/info/info.component.ts
@@ -13,17 +13,18 @@ export class InfoComponent implements OnInit {
 
   jugadores;
 
+  terminoPartida: boolean = false;
+
   constructor(private webSocket: WebsocketService, private api: ApiService,) { }
 
   ngOnInit() {
-
     this.api.getJugadores().subscribe((jugadores) => {
       this.jugadores = jugadores;
       this.jugadores.sort((a,b) => {
         if(a.puntaje < b.puntaje)
-          return 1
+        return 1
         if(a.puntaje > b.puntaje)
-          return -1
+        return -1
         return 0;
       });
     })
@@ -32,9 +33,9 @@ export class InfoComponent implements OnInit {
       this.jugadores.push(data);
       this.jugadores.sort((a,b) => {
         if(a.puntaje < b.puntaje)
-          return 1
+        return 1
         if(a.puntaje > b.puntaje)
-          return -1
+        return -1
         return 0;
       });
     });
@@ -42,18 +43,31 @@ export class InfoComponent implements OnInit {
     this.webSocket.listen('pasoPregunta').subscribe((data) => {
       this.api.getJugadores().subscribe((jugadores) => {
         this.jugadores = jugadores;
+        this.terminoPartida = this.checkFinPartida();
       })
     })
-
+    
     this.webSocket.listen('salirSala').subscribe((idJugador) => {
       // Busco el jugador que se fue por ID y lo elimino de la lista.
       this.jugadores.splice(this.jugadores.findIndex(j => j.idJugador == idJugador), 1);
     })
-
+    
     // Evento que se dispara cuando un juego fue inicializado
     this.webSocket.listen('inicioJuego').subscribe((idJuego) => {
       this.inicioJuego = true;
     })
+
+  }
+
+  checkFinPartida() {
+    // Chequeo que todos los jugadores hayan terminado de jugar para anunciar al ganador final
+    let terminaron = true;
+    this.jugadores.forEach(jugador => {
+      if(jugador.jugando != -1)
+        terminaron = false;
+    });
+    console.log(terminaron);
+    return terminaron;
   }
 
 }
